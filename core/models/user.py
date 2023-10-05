e7e1106840c10b1ddc5aff0f53f61f088299f839
Dmitiy <sava777cool@gmail.com>
2023-10-05 18:18:35 +0300

    Add profile.

--- core/models/user.py
+++ core/models/user.py
@@ -6,8 +6,11 @@ from .base import Base
 
 if TYPE_CHECKING:
     from .post import Post
+    from .profile import Profile
 
 
 class User(Base):
     username: Mapped[str] = mapped_column(String(32), unique=True)
+
     pasts: Mapped[list["Post"]] = relationship(back_populates="user")
+    profile: Mapped[list["Profile"]] = relationship(back_populates="user")
